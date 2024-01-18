f25594a3ffaf66a59b418765a80baffff693762d
Gene Bogdanovich <genebogdanovich@gmail.com>
2024-01-18 09:00:26 +0300

    Reproduce the crash.

--- FontPicker/ContentView.swift
+++ FontPicker/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 // MARK: - CustomFont
 
-enum CustomFont: String, CaseIterable, Identifiable {
+enum CustomFont: String, CaseIterable, Identifiable, Codable {
     case baskerville = "Baskerville"
     case chalkboardSE = "ChalkboardSE-Regular"
     
@@ -29,15 +29,32 @@ enum CustomFont: String, CaseIterable, Identifiable {
 
 // MARK: - AppFont
 
-enum AppFont: Hashable {
+enum AppFont: Hashable, Codable {
     case `default`
     case custom(font: CustomFont)
+}
+
+#warning("Try Data")
+extension AppFont: RawRepresentable {
+    init?(rawValue: String) {
+        let data = rawValue.data(using: .utf8)!
+        self = try! JSONDecoder().decode(AppFont.self, from: data)
+    }
+    
+    var rawValue: String {
+        let data = try! JSONEncoder().encode(self)
+        return String(decoding: data, as: UTF8.self)
+    }
+    
+    typealias RawValue = String
+    
+    
 }
 
 // MARK: - ContentView
 
 struct ContentView: View {
-    @State private var selectedFont: AppFont = .custom(font: .baskerville)
+    @AppStorage("selectedFont") private var selectedFont: AppFont = .custom(font: .baskerville)
     
     var body: some View {
         VStack {
